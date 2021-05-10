f8ea4807fbd4a3bec8d4be9d0e00fb619d948864
estela <estela.flores.antony.rober.oswaldo@iisgalvanimi.edu.it>
2021-05-11 00:53:18 +0200

    quasi

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react'
+import ReactDom from 'react-dom'
+
+import List from './containers/List'
+
+import 'bootswatch/dist/lux/bootstrap.min.css';
+
+const App = () => {
+    return (
+        <Fragment>
+            <nav className="navbar navbar-dark bg-dark border-bottom border-white">
+                <a href="/" className="navbar-brand">
+                    Watcher
+                </a>
+
+            </nav>
+            <main className="bg-dark">
+                <div className="container">
+                    <List />
+                </div>
+            </main>
+        </Fragment>
+    )
+}
+
+ReactDom.render(<App />, document.getElementById('root'))
+
